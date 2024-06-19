905f394407d725272e7065cf24f4297c029e9e45
FlavioCorp <flaviomantecoso@gmail.com>
2024-06-19 17:30:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.getElementById('myModal');
-    var span = document.getElementsByClassName('close')[0];
-
-    modal.style.display = "block";
-
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
     window.addEventListener("hashchange", function() {
   var seccionActual = window.location.hash.substring(1); // Obtenemos el ID de la sección actual sin el "#" inicial
   if (seccionActual === "seccion2") {
